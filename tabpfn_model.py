5ffacd092419218ab1a010c1686f3bd036c987f8
João Gabriel Silveira <joaobmasilveira@ufmg.br>
2026-05-03 19:31:02 -0300

    Fixed embedding dimensions

--- tabpfn_model.py
+++ tabpfn_model.py
@@ -6,11 +6,19 @@ import numpy as np
 from database import impute_data, normalize_data
 from filepaths import get_env_path
 
+BATCH_SIZE = 512
+
+class TabPFNEvalConfig:
+    rng_seed: int = 42
+    tabpfn_model_name: str = 'dr_tabpfn'
+    batch_size_predict: int = BATCH_SIZE
+    run_id: str = "demo_tabpfn_step1"
+
 TRAINING_EMBEDDING_FILE = get_env_path('models/tabpfn/tabpfn_emb_train.npy')
 TEST_EMBEDDING_FILE = get_env_path('models/tabpfn/tabpfn_emb_test.npy')
 PRED_BIN_FILE = get_env_path('models/tabpfn/y_pred_bin.npy')
 PRED_PROB_FILE = get_env_path('models/tabpfn/y_pred_prob.npy')
-BATCH_SIZE = 512
+
 
 def get_tabpfn_model(arrays: dict[str, np.ndarray], get_embeddings=False, get_pred=False) -> (TabPFNClassifier |
                                                                                               tuple[
@@ -86,6 +94,8 @@ def get_tabpfn_model(arrays: dict[str, np.ndarray], get_embeddings=False, get_pr
                                                                           X_train_normalized[i:i + BATCH_SIZE, :],
                                                                           data_source='train')
                     train_emb_list.append(train_embeddings)
+                    if train_embeddings.ndim == 3:
+                        train_embeddings = np.mean(train_embeddings, axis=0)
                 train_embeddings = np.concatenate(train_emb_list, axis=0)
 
                 with open(TRAINING_EMBEDDING_FILE, 'wb') as train_emb:
@@ -100,6 +110,8 @@ def get_tabpfn_model(arrays: dict[str, np.ndarray], get_embeddings=False, get_pr
                     test_embeddings = embedding_extractor.get_embeddings(X_train_normalized, y_train_normalized,
                                                                          X_test_normalized[i:i + BATCH_SIZE, :],
                                                                          data_source='test')
+                    if test_embeddings.ndim == 3:
+                        test_embeddings = np.mean(test_embeddings, axis=0)
                     test_emb_list.append(test_embeddings)
                 test_embeddings = np.concatenate(test_emb_list, axis=0)
 
@@ -107,9 +119,6 @@ def get_tabpfn_model(arrays: dict[str, np.ndarray], get_embeddings=False, get_pr
                     np.save(test_emb, test_embeddings)
             else:
                 test_embeddings = np.load(TEST_EMBEDDING_FILE)
-
-        train_embeddings = np.mean(train_embeddings, axis=0)
-        test_embeddings = np.mean(test_embeddings, axis=0)
 
         return clf, train_embeddings, test_embeddings, {
             'X_train': X_train, 'X_test': X_test,
@@ -127,3 +136,20 @@ def get_tabpfn_model(arrays: dict[str, np.ndarray], get_embeddings=False, get_pr
         }
 
     return clf
+
+# def fit_dr_tabpfn(X_train: np.ndarray, y_train: np.ndarray, train_years: np.ndarray, eval_cfg: TabPFNEvalConfig):
+#     X_train_np = np.ascontiguousarray(X_train.astype(np.float32, copy=True))
+#     y_train_np = np.ascontiguousarray(y_train.astype(np.float32, copy=True))
+#
+#     # permitir que o tabfpn modifique as entradas, se necessário
+#     X_train_np.setflags(write=True)
+#     y_train_np.setflags(write=True)
+#
+#     train_years = np.asarray(train_years).astype(int)
+#     # um index (dominio) por ano
+#     year_to_domain_train = {y: i for i, y in sorted(set(enumerate(train_years)))}
+#     dist_shift_domain_train_np = np.ascontiguousarray(
+#         np.array([year_to_domain_train[int(y)] for y in train_years], dtype=np.int64)
+#     )
+
+    
